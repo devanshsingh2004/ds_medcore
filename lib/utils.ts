5f5d6ad7d11de0238a8e73ac1f7b470be0337281
devanshsingh2004 <singhdevansh4747@gmail.com>
2024-09-07 15:07:53 +0530

    first commit

---
+++ lib/utils.ts
@@ -0,0 +1,80 @@
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export const parseStringify = (value: any) => JSON.parse(JSON.stringify(value));
+
+export const convertFileToUrl = (file: File) => URL.createObjectURL(file);
+
+export const formatDateTime = (dateString: Date | string, timeZone: string = Intl.DateTimeFormat().resolvedOptions().timeZone) => {
+  const dateTimeOptions: Intl.DateTimeFormatOptions = {
+
+    month: "short", 
+    day: "numeric", 
+    year: "numeric", 
+    hour: "numeric",
+    minute: "numeric", 
+    hour12: true, 
+    timeZone: timeZone,
+  };
+
+  const dateDayOptions: Intl.DateTimeFormatOptions = {
+    weekday: "short",
+    year: "numeric", 
+    month: "2-digit", 
+    day: "2-digit", 
+    timeZone: timeZone, 
+  };
+
+  const dateOptions: Intl.DateTimeFormatOptions = {
+    month: "short", 
+    year: "numeric",
+    day: "numeric", 
+    timeZone: timeZone, 
+  };
+
+  const timeOptions: Intl.DateTimeFormatOptions = {
+    hour: "numeric", 
+    minute: "numeric", 
+    hour12: true,
+    timeZone: timeZone,
+  };
+
+  const formattedDateTime: string = new Date(dateString).toLocaleString(
+    "en-US",
+    dateTimeOptions
+  );
+
+  const formattedDateDay: string = new Date(dateString).toLocaleString(
+    "en-US",
+    dateDayOptions
+  );
+
+  const formattedDate: string = new Date(dateString).toLocaleString(
+    "en-US",
+    dateOptions
+  );
+
+  const formattedTime: string = new Date(dateString).toLocaleString(
+    "en-IN",
+    timeOptions
+  );
+
+  return {
+    dateTime: formattedDateTime,
+    dateDay: formattedDateDay,
+    dateOnly: formattedDate,
+    timeOnly: formattedTime,
+  };
+};
+
+export function encryptKey(passkey: string) {
+  return btoa(passkey);
+}
+
+export function decryptKey(passkey: string) {
+  return atob(passkey);
+}
---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://b1ad2e43a56b8418801fb865be703a97@o4507908393730048.ingest.us.sentry.io/4507908397596672",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
